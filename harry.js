fd1c054e4206dbb308cf1d8cbe1e6cace01181eb
Anna Nykiel <nykielanna94@gmail.com>
2023-03-30 11:43:36 +0200

    Initial commit

---
+++ harry.js
@@ -0,0 +1,12 @@
+console.log("Cześć!");
+
+let przycisk =document.querySelector(".przycisk");
+let img =document.querySelector(".img");
+przycisk.addEventListener("click", () => {
+
+img.remove();
+
+
+});
+
+console.log(przycisk);
